e468ad531c0c9a332123206efe59195b1b91ae3e
Nguyễn Quang Quyền <quyennguyenquang@192.168.102.37>
2025-06-02 22:33:14 +0700

    add ui products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -37,6 +37,7 @@ import {
 import { ForgotPassword } from "./pages/forgotPassword";
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
+import { ProductsCreate, ProductsEdit, ProductsList, ProductsShow } from "./pages/products";
 
 function App() {
   return (
@@ -47,7 +48,7 @@ function App() {
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                dataProvider={dataProvider("https://server-cua-quyennq.onrender.com")}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
@@ -68,6 +69,16 @@ function App() {
                     create: "/categories/create",
                     edit: "/categories/edit/:id",
                     show: "/categories/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
+                  },
+                   {
+                    name: "products",
+                    list: "/products",
+                    create: "/products/create",
+                    edit: "/products/edit/:id",
+                    show: "/products/show/:id",
                     meta: {
                       canDelete: true,
                     },
@@ -111,6 +122,12 @@ function App() {
                       <Route path="create" element={<CategoryCreate />} />
                       <Route path="edit/:id" element={<CategoryEdit />} />
                       <Route path="show/:id" element={<CategoryShow />} />
+                    </Route>
+                      <Route path="/products">
+                      <Route index element={<ProductsList />} />
+                      <Route path="create" element={<ProductsCreate />} />
+                      <Route path="edit/:id" element={<ProductsEdit />} />
+                      <Route path="show/:id" element={<ProductsShow />} />
                     </Route>
                     <Route path="*" element={<ErrorComponent />} />
                   </Route>
