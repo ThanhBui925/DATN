7f733ee9e63867fab8100db426d1955f244c960b
thanhbui925 <thanh.buitrung21@gmail.com>
2025-06-29 15:38:28 +0700

    add send mail success password

---
+++ backend/resources/views/emails/password-reset-success.blade.php
@@ -0,0 +1,7 @@
+<h2>Chào {{ $user->name }},</h2>
+
+<p>Bạn đã thay đổi mật khẩu thành công vào lúc {{ now()->format('H:i d/m/Y') }}.</p>
+
+<p>Nếu bạn không thực hiện thao tác này, vui lòng liên hệ bộ phận hỗ trợ ngay lập tức.</p>
+
+<p>Trân trọng,<br>Đội ngũ hỗ trợ</p>
