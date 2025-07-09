4a9c22ea9f83c304b545c66859b5436512ec11b6
TrangPT28 <trangptph53066@gmail.com>
2025-07-09 14:30:52 +0700

    fetch api categories at header page and shop page

--- frontend/src/pages/shop.tsx
+++ frontend/src/pages/shop.tsx
@@ -1,6 +1,9 @@
 import {Sidebar} from "../components/shop/Sidebar";
 import {ProductList} from "../components/shop/ProductList";
 import {Breadcrumb} from "../components/Breadcrumb";
+import {useEffect, useState} from "react";
+import {axiosInstance} from "../utils/axios";
+import {notification} from "antd";
 
 export const Shop = () => {
     const breadcrumb = {
@@ -10,13 +13,14 @@ export const Shop = () => {
     return (
         <>
             <Breadcrumb {...breadcrumb} />
-            <div className="content-wraper">
+            <div className="content-wraper mb-5">
                 <div className="container-fluid">
                     <div className="row">
-                        <Sidebar />
-                        <ProductList />
+                        <Sidebar/>
+                        <ProductList/>
                     </div>
                 </div>
-            </div>        </>
+            </div>
+        </>
     )
 }
