056f610ab1f11d2fabe47f8884f35e187162ed1c
thanhbui925 <thanh.buitrung21@gmail.com>
2025-09-04 23:28:01 +0700

    add_mail_reject

--- backend/app/Http/Controllers/api/OrderController.php
+++ backend/app/Http/Controllers/api/OrderController.php
@@ -394,6 +394,8 @@ public function show($id)
             }
         }
 
+
+
         //Nếu trạng thái là return_rejected, xóa toàn bộ thông tin trả hàng và lưu lý do từ chối
         if ($newStatus === 'return_rejected') {
             $reason = $request->input('reject_reason');
@@ -412,7 +414,17 @@ public function show($id)
                 $returnOrder->reason_for_refusal = $reason;
                 $returnOrder->save();
             }
+            //Gửi mail thông báo từ chối trả hàng cho khách
+            try {
+                \Mail::to($order->recipient_email)->queue(new \App\Mail\ReturnRejectedMail($order));
+            } catch (\Exception $e) {
+                Log::error("Failed to queue return rejected email for order ID {$order->id}: " . $e->getMessage());
+            }   
+
         }
+
+        //gửi mail thông báo có yêu cầu trả hàng cho admin
+            
 
         // if ($newStatus === 'refunded') {
         //     $transactionCode = $request->input('transaction_code');
